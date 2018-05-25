87ddc1b4ab1280d96bd9492ce927e76dd6b11d45
stasinopoulos <an.stasinopoulos@gmail.com>
2018-05-25 07:34:39 +0300

    Minor update

--- src/core/tamper/hexencode.py
+++ src/core/tamper/hexencode.py
@@ -13,6 +13,7 @@ the Free Software Foundation, either version 3 of the License, or
 For more see the file 'readme/COPYING' for copying permission.
 """
 
+import sys
 import urllib
 from src.utils import settings
 
@@ -28,6 +29,13 @@ if not settings.TAMPER_SCRIPTS[__tamper__]:
   settings.TAMPER_SCRIPTS[__tamper__] = True
 
 def encode(payload):
+  if settings.WHITESPACE[0] == "+":
+    err_msg = "Tamper script '" +  __tamper__  + "' is unlikely to work combined with the tamper script 'space2plus'."
+    if settings.VERBOSITY_LEVEL == 0:
+      print ""
+    print settings.print_critical_msg(err_msg) 
+    raise SystemExit()
+
   payload = urllib.unquote(payload)
   payload = payload.encode("hex")
   return payload
