13048ae1ff10211ed4185b3279ef76416a899da0
stasinopoulos <an.stasinopoulos@gmail.com>
2018-07-18 07:50:00 +0300

    Fixes https://github.com/commixproject/commix/issues/328

--- src/core/tamper/space2ifs.py
+++ src/core/tamper/space2ifs.py
@@ -13,6 +13,7 @@ the Free Software Foundation, either version 3 of the License, or
 For more see the file 'readme/COPYING' for copying permission.
 """
 
+import sys
 from src.utils import settings
 
 """
