6065c1f1763938553b273b7dfac6218ae86d851a
stasinopoulos <an.stasinopoulos@gmail.com>
2019-07-11 10:22:51 +0300

    Trivial update

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -155,7 +155,7 @@ APPLICATION = "commix"
 DESCRIPTION_FULL = "Automated All-in-One OS Command Injection and Exploitation Tool"
 DESCRIPTION = "The command injection exploiter"
 AUTHOR  = "Anastasios Stasinopoulos"
-VERSION_NUM = "3.0.8"
+VERSION_NUM = "3.0.9"
 STABLE_VERSION = False
 if STABLE_VERSION:
   VERSION = "v" + VERSION_NUM[:3] + "-stable"
