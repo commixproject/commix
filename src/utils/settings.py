52fa62041873f54b5d8d3d466d2afcd18e0a1bb1
stasinopoulos <an.stasinopoulos@gmail.com>
2020-02-14 08:01:58 +0200

    fixes https://github.com/commixproject/commix/issues/508

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -160,7 +160,7 @@ APPLICATION = "commix"
 DESCRIPTION_FULL = "Automated All-in-One OS Command Injection and Exploitation Tool"
 DESCRIPTION = "The command injection exploiter"
 AUTHOR  = "Anastasios Stasinopoulos"
-VERSION_NUM = "3.1.13"
+VERSION_NUM = "3.1.14"
 STABLE_VERSION = False
 if STABLE_VERSION:
   VERSION = "v" + VERSION_NUM[:3] + "-stable"
