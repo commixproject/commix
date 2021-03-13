01701f9f3bc229cc062e9ace882fd5a2428f808a
stasinopoulos <an.stasinopoulos@gmail.com>
2021-03-13 09:48:11 +0200

    Minor update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 setup(
       name='commix',
       version='3.2-dev',
-      description='Automated All-in-One OS command injection and exploitation tool.',
+      description='Automated All-in-One OS Command Injection Exploitation Tool.',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
       author='Anastasios Stasinopoulos',
--- src/utils/settings.py
+++ src/utils/settings.py
@@ -200,10 +200,10 @@ The global variables.
 """
 # About
 APPLICATION = "commix"
-DESCRIPTION_FULL = "Automated All-in-One OS Command Injection and Exploitation Tool"
+DESCRIPTION_FULL = "Automated All-in-One OS Command Injection Exploitation Tool"
 DESCRIPTION = "The command injection exploiter"
 AUTHOR  = "Anastasios Stasinopoulos"
-VERSION_NUM = "3.2.80"
+VERSION_NUM = "3.2.81"
 STABLE_VERSION = False
 if STABLE_VERSION:
   VERSION = "v" + VERSION_NUM[:3] + "-stable"
