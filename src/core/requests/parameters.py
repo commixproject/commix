54b625bb2724c8e648c598e6899a2aa57dacd067
stasinopoulos <an.stasinopoulos@gmail.com>
2021-05-07 09:03:44 +0300

    Minor refactoring

--- src/core/requests/parameters.py
+++ src/core/requests/parameters.py
@@ -47,9 +47,9 @@ def do_GET_check(url, http_request_method):
       if menu.options.level == 2 :
         return False
       else: 
-        err_msg = "No parameter(s) found for testing in the provided data. "
-        err_msg += "You must specify the testable parameter or "
-        err_msg += "try to increase '--level' values to perform more tests." 
+        err_msg = "No parameter(s) found for testing on the provided target URL. "
+        err_msg += "You must specify the testable parameter(s) and/or "
+        err_msg += "try to increase '--level' values to perform more tests (i.e 'User-Agent', 'Referer', 'Host', 'Cookie' etc)." 
         print(settings.print_critical_msg(err_msg))
         raise SystemExit()
     elif menu.options.shellshock:
