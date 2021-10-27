bd9407e9a4e8c92e0081fff47be47b3b021f1758
stasinopoulos <an.stasinopoulos@gmail.com>
2021-10-27 09:03:51 +0300

    Minor refactoring

--- src/utils/common.py
+++ src/utils/common.py
@@ -178,7 +178,7 @@ def unhandled_exception():
     print(settings.print_critical_msg(err_msg))
     raise SystemExit()
 
-  elif "MemoryError" in exc_msg:
+  elif any(_ in exc_msg for _ in ("MemoryError", "Cannot allocate memory")):
     err_msg = "Memory exhaustion detected."
     print(settings.print_critical_msg(err_msg))
     raise SystemExit()
