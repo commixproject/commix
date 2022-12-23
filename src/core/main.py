17ca1f025833ebf6cd13afe41b318a0a27d5ffa0
stasinopoulos <an.stasinopoulos@gmail.com>
2022-12-23 09:10:55 +0200

    Fixes https://github.com/commixproject/commix/issues/798

--- src/core/main.py
+++ src/core/main.py
@@ -293,7 +293,9 @@ def url_response(url):
   response = examine_request(request, url)
   # Check for URL redirection
   if not menu.options.ignore_redirects:
-    url = redirection.do_check(request, url)
+    redirect_url = redirection.do_check(request, url)
+    if redirect_url is not None:
+      url = redirect_url
   return response, url
 
 """
