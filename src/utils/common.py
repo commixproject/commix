396c67bb643f08ff918cac60c847bd4dbfa9cf74
stasinopoulos <an.stasinopoulos@gmail.com>
2023-02-08 08:08:37 +0200

    Fixes https://github.com/commixproject/commix/issues/811

--- src/utils/common.py
+++ src/utils/common.py
@@ -307,7 +307,7 @@ def unhandled_exception():
 
   elif all(_ in exc_msg for _ in ("SyntaxError: Non-ASCII character", ".py on line", "but no encoding declared")) or \
        any(_ in exc_msg for _ in ("source code string cannot contain null bytes", "No module named")) or \
-       any(_ in exc_msg for _ in ("ImportError", "ModuleNotFoundError", "Can't find file for module")):
+       any(_ in exc_msg for _ in ("ImportError", "ModuleNotFoundError", "<frozen", "Can't find file for module")):
     err_msg = "Invalid runtime environment ('" + exc_msg.split("Error: ")[-1].strip() + "')."
     print(settings.print_critical_msg(err_msg))
     raise SystemExit()
