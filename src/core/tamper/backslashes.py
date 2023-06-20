27a6f0d884d3ccae510fe4ee81699aebafa00fec
stasinopoulos <an.stasinopoulos@gmail.com>
2023-06-20 07:28:59 +0300

    Minor update regarding commit https://github.com/commixproject/commix/commit/edb2229ae6e4f0b477712b0752aca86a95df7252

--- src/core/tamper/backslashes.py
+++ src/core/tamper/backslashes.py
@@ -32,7 +32,7 @@ def tamper(payload):
   def add_back_slashes(payload):
     settings.TAMPER_SCRIPTS[__tamper__] = True
     obf_char = "\\"
-    payload = re.sub(r'([b-zD-Z])', r"\\\1", payload)
+    payload = re.sub(r'([b-zD-Z])', lambda x: obf_char + x[0], payload)
     for word in settings.IGNORE_TAMPER_TRANSFORMATION:
       _ = obf_char.join(word[i:i+1] for i in range(-1, len(word), 1))
       if _ in payload:
