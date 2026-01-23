bef11679c97a3959125e3c3efcb304ca8f3ae26b
stasinopoulos <an.stasinopoulos@gmail.com>
2026-01-23 21:49:10 +0200

    Update copyright year

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -3,7 +3,7 @@
 
 """
 This file is part of Commix Project (https://commixproject.com).
-Copyright (c) 2014-2025 Anastasios Stasinopoulos (@ancst).
+Copyright (c) 2014-2026 Anastasios Stasinopoulos (@ancst).
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -262,7 +262,7 @@ DESCRIPTION_FULL = "Automated All-in-One OS Command Injection Exploitation Tool"
 DESCRIPTION = "The command injection exploiter"
 AUTHOR  = "Anastasios Stasinopoulos"
 VERSION_NUM = "4.2"
-REVISION = "4"
+REVISION = "5"
 STABLE_RELEASE = False
 VERSION = "v"
 if STABLE_RELEASE:
@@ -272,7 +272,7 @@ else:
   VERSION = VERSION + VERSION_NUM + ".dev" + REVISION
   COLOR_VERSION = Style.UNDERLINE + Fore.WHITE + VERSION + Style.RESET_ALL
 
-YEAR = "2014-2025"
+YEAR = "2014-2026"
 AUTHOR_X_ACCOUNT = "@ancst"
 APPLICATION_URL = "https://commixproject.com"
 APPLICATION_X_ACCOUNT = "@commixproject"
